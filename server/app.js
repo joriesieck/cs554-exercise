02ccd110cc77c07ef124886bbfeff088354f8de8
Manas Acharekar <manas.acharekar98@gmail.com>
2021-12-01 19:13:09 -0500

    changes to backend

---
+++ server/app.js
@@ -0,0 +1,37 @@
+const app = require('express');
+const http = require('http').createServer(app);
+var io = require('socket.io')(http, {
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+	},
+});
+
+io.on('connection', (socket) => {
+	/* socket object may be used to send specific messages to the new connected client */
+
+	console.log('new client connected');
+	socket.emit('connection', null);
+	socket.on('user_join', ({ name, room }) => {
+		this.username = name;
+		this.room = room;
+		console.log(name);
+		console.log(room);
+		socket.join(room);
+		socket.to(room).emit('user_join', name);
+	});
+
+	socket.on('message', ({ name, message, room }) => {
+		console.log(name, message);
+		io.to(room).emit('message', { name, message });
+	});
+
+	socket.on('disconnect', () => {
+		console.log('Disconnect Fired');
+		//socket.broadcast.emit('user_leave', `${this.username} has left`);
+	});
+});
+
+http.listen(4000, () => {
+	console.log(`listening on *:${4000}`);
+});
